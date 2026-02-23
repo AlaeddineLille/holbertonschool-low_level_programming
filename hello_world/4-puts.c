c8e2fc9f86fb0e9af24cbcf5ff9388f2487aa343
AlaeddineLille <12987@holbertonstudents.com>
2026-02-23 13:17:40 +0000

    4-puts.c test puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
+/*
+ * puts programming is like building a multilingual puzzle
+ */
 
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+	puts("\"Programming is like building a multilingual puzzle\"");
+
+	return (0);
 }
