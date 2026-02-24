b7852373bb8a701dbb540c02d7cbd98d8ebfdfab
AlaeddineLille <12987@holbertonstudents.com>
2026-02-24 14:59:37 +0000

    putchar the numbers of base 16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	char la;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n + '0');
+	}
+	for (la = 'a'; la <= 'f'; la++)
+	{
+		putchar(la);
+	}
+	putchar('\n');
+
+	return (0);
+}
