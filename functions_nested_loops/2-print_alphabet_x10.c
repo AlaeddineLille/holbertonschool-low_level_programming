dd3664106c557b8e2a8c38a60e3a68d7954388d3
AlaeddineLille <12987@holbertonstudents.com>
2026-02-27 11:30:02 +0100

    print alphabet 10 time

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 time
+ *
+ * Return: nothing
+ */
+
+void print_alphabet_x10(void);
+
+{
+	int i;
+	char letter;
+
+	if (i <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	else
+	_putchar('\n');
+	}
+}
